e5e7336b99540cd66080cd657a28f3e7be5016c9
Jeong Hyeonyong <jhy5317@gmail.com>
2018-01-24 01:52:41 +0900

    first commit

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 var express = require('express');
 var axios = require('axios');
+var multer = require('multer');
+var fileValidation = true;
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.body.studentnumber + "_timetable")
+    }
+});
 
 var app = express();
-
-var session = require('express-session');
-app.set('trust proxy', 1) // trust first proxy
-app.use(session({
-  secret: '201712131633',
-  resave: false,
-  saveUninitialized: true,
-}));
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://jhy5317:mobile5317@ds123896.mlab.com:23896/2018miro');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('we are connected!');
+var upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb){
+    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpg' && file.mimetype !== 'image/jpeg' && file.mimetype !== ""){
+      fileValidation = false;
+      return cb(null, false);
+    }
+      cb(null, true);
+    },
+  limits: {filesize: 1024 * 1024}
 });
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://jhy5317:mobile5317@ds123896.mlab.com:23896/2018miro', {useMongoClient: true}, function(req, res){
+  console.log('We are Connected!')
+});
+mongoose.Promise = global.Promise;
 
 var static = require('serve-static');
 app.use(express.static(__dirname + '/public'));
@@ -35,47 +46,71 @@ app.use(bodyParser.urlencoded({extended: false}));
 const Apply = require('./models/apply');
 
 app.get('/', function(req, res){
-  res.render('info.ejs',{boolean:false, checkResult:""});
+  res.render('info.ejs', {applyCheck:false, checkResult:false, wrongFile:false, checking:false});
 });
 
 app.get('/checkingApply', function(req, res){
-  res.render('checkingApply.ejs');
+  res.render('checkingApply.ejs')
 });
 
 app.post('/check', function(req, res){
   Apply.find({studentnumber: req.body.studentnumber}, function(err, result){
     if(err) throw err;
     if(result.length > 0){
-      res.render('info.ejs',{boolean:false, checkResult: 1});
+      res.json({success: true,
+                name: result[0].name,
+                major: result[0].major,
+                studentnumber: req.body.studentnumber,
+                grade: result[0].grade,
+                phonenumber: result[0].phonenumber,
+              });
     } else{
-      res.render('info.ejs',{boolean:false, checkResult: 2});
+      res.json({success: false});
     }
   });
 });
 
-app.post('/applyMiro', function(req, res){
-  const apply = new Apply({
-    name: req.body.name,
-    major: req.body.major,
-    studentnumber: req.body.studentnumber,
-    grade: req.body.grade,
-    motive: req.body.motive,
-    phonenumber: "0" + req.body.phonenumber
-  });
-
-  Apply.find({studentnumber: req.body.studentnumber}, function(err, result){
-    if(err) throw err;
-      if(result.length > 0){
-      //검색결과 존재
-          res.render('info.ejs',{boolean: true});
-      } else{
-        apply.save(function(err){
-          if(err) throw err;
-        });
-        res.redirect('/');
-      }
-  });
+app.post('/applyMiro', upload.single('timetable'), function(req, res){
+  console.log(req.body);
+  if(req.body.checking !== "확인"){
+    res.render('info.ejs',{applyCheck:false, checkResult:false, wrongFile:false, checking:true})
+  } else {
+    if(fileValidation !== false){
+      const apply = new Apply({
+        name: req.body.name,
+        major: req.body.major,
+        studentnumber: req.body.studentnumber.toString(),
+        grade: req.body.grade,
+        motive: req.body.motive,
+        phonenumber: "010"+req.body.phonenumber.toString()
+      });
+      Apply.find({studentnumber: req.body.studentnumber}, function(err, result){
+        if(err) {
+          throw err;
+        } else {
+          if(result.length > 0){
+            res.render('info.ejs', {applyCheck:true, checkResult:false, wrongFile:false, checking:false})
+          } else{
+            apply.save(function(err){
+              if(err) throw err;
+              res.redirect('/');
+            })
+          }
+        }
+      })
+    }else{
+      res.render('info.ejs',{applyCheck:false, checkResult:false, wrongFile:true, checking:false});
+    }
+  }
 });
+
+app.get('/ggg', function(req, res){
+  res.redirect('ggg.html')
+})
+
+app.get('/location', function(req, res){
+  res.redirect('contact.html')
+})
 
 app.listen(3000, function(req, res){
   console.log('Server is Working.')
